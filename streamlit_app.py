04c38b2eb9e28ab06ce498f448d049901de55d22
Max Humpert <107491705+sfc-gh-mhumpert@users.noreply.github.com>
2023-01-17 18:59:59 +0100

    Update streamlit_app.py
    updated text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
-streamlit.title('Hello World')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Tasty Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
