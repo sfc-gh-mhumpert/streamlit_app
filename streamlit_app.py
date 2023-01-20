ccfb068de5ee2895cdae497f371e8e96300fad9d
Max Humpert <107491705+sfc-gh-mhumpert@users.noreply.github.com>
2023-01-20 15:25:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+import urllib.error import URLError
 
 
 streamlit.title('My Parents New Healthy Diner')
@@ -25,7 +28,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # write your own comment -what does the next line do? 
@@ -33,7 +35,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -43,3 +46,5 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice = streamlit.text_input('What fruit would you like to add to the list?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
